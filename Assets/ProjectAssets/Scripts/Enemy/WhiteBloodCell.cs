d0936b9412eca0a869897002d60553de6a1540b7
Mark <freak_mania@hotmail.com>
2021-02-19 19:52:32 +0100

    Enemy behavior

--- Assets/ProjectAssets/Scripts/Enemy/WhiteBloodCell.cs
+++ Assets/ProjectAssets/Scripts/Enemy/WhiteBloodCell.cs
@@ -2,28 +2,101 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent (typeof (Rigidbody))]
 public class WhiteBloodCell : MonoBehaviour, Icell, IDamagable
 {
-   // [Header ("IDamagable interface")]
+
+    [Header ("Movement")]
+    float currentSpeed;
+    [SerializeField] float speed = 1;
+    Vector3 moveDir;
+    Vector3 lookDir;
+
+    Rigidbody rb;
+
+    [SerializeField] Transform followTarget;
+    [SerializeField] float followThreshold = 1f;
+
+    [Header ("IDamagable interface")]
+    [SerializeField] int setHP;
     public int hp { get; set; }
+
+    [SerializeField] private int setDmg = 1;
     public int dmg { get; set; }
 
-   // [Header ("Icell interface")]
+    public enum CellType { Bacteria, WhiteBloodCell }
+
+    [Header ("Icell interface")]
+    [SerializeField] CellType setCellType = CellType.WhiteBloodCell;
     public byte cellType { get; set; }
-    //public GameObject masterCell { get; set; }
-    public float energy { get; set; }
-    public float energyDrain { get; set; }
 
-    public int splitAmount { get; set; }
-    public AudioSource playSplitSound { get; set; }
-    public AudioClip[] splitSound { get; set; }
+    [HideInInspector] public float energy { get; set; }
 
+    [HideInInspector] public float energyDrain { get; set; }
+
+    [HideInInspector] public int splitAmount { get; set; }
+
+    [HideInInspector] public AudioSource playSplitSound { get; set; }
+
+    [HideInInspector] public AudioClip[] splitSound { get; set; }
+
+    void Start ()
+    {
+        hp = setHP;
+        dmg = setDmg;
+        cellType = (byte) setCellType;
+        //        print (cellType);
+
+        rb = GetComponent<Rigidbody> ();
+
+    }
+
+    //empty functions, they do nothing but the interface will give a bitchfit if they are not here.
+    #region 
     public void Split ()
     {
 
     }
     public void DrainEnergy ()
     {
+
+    }
+
+    #endregion
+
+    void Update ()
+    {
+        if (followTarget == null)
+        {
+            return;
+        }
+
+        currentSpeed = speed;
+        Vector3 forward = new Vector3 ();
+        float distance = Vector3.Distance (transform.position, followTarget.position);
+        //  if (distance >= followThreshold)
+        {
+            forward = transform.forward * distance * currentSpeed;
+        }
+        forward = transform.forward * currentSpeed;
+        lookDir = followTarget.position - transform.position;
+
+        moveDir = forward;
+
+    }
+
+    void FixedUpdate ()
+    {
+        Movement ();
+
+    }
+
+    void Movement ()
+    {
+
+        //de berekende move en rotation vectoren worden hier in FixedUpdate toegepast.
+        rb.rotation = Quaternion.LookRotation (lookDir);
+        rb.velocity = moveDir;
 
     }
 
@@ -39,6 +112,35 @@ public class WhiteBloodCell : MonoBehaviour, Icell, IDamagable
     public void Kill ()
     {
         Destroy (gameObject);
+    }
+
+    public void OnTriggerEnter (Collider col)
+    {
+
+        IDamagable getIdamagable = col.GetComponent<IDamagable> ();
+        Icell getIcell = col.GetComponent<Icell> ();
+
+        //only deal damage to a damagable object if it is not part of our celltype
+        if (getIdamagable != null && getIcell != null)
+        {
+
+            if (getIcell.cellType != cellType)
+            {
+                followTarget = col.transform;
+
+                print (followTarget);
+
+                //   getIdamagable.ApplyDamage (dmg);
+//                print ("I dealt damage to-" + col.name);
+
+            }
+
+        }
+
+    }
+    public void OnTriggerExit ()
+    {
+        followTarget = null;
     }
 
 }
