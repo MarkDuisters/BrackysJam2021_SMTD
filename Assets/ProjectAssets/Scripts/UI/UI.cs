2e83de618dcce00f8065d1d3a7ed4d415312b9c9
SirSalt <stijn.bellemans@gmail.com>
2021-02-20 21:21:04 +0100

    Pause menu

--- Assets/ProjectAssets/Scripts/UI/UI.cs
+++ Assets/ProjectAssets/Scripts/UI/UI.cs
@@ -6,9 +6,34 @@ using UnityEngine.SceneManagement;
 public class UI : MonoBehaviour
 {
 
+    public GameObject pauseMenu;
+
+    void Update()
+    {
+        PauseMenu();
+    }
+
     public void LoadScene()
     {
         SceneManager.LoadScene(1);
+    }
+
+    public void PauseMenu()
+    {
+        if (Input.GetButtonDown("Pause"))
+        {
+            GameManager.instance.isPaused = !GameManager.instance.isPaused;
+            pauseMenu.SetActive(GameManager.instance.isPaused);
+
+            if (GameManager.instance.isPaused)
+            {
+                GameManager.instance.PauseGame();
+            }
+            else
+            {
+                GameManager.instance.UnPauseGame();
+            }
+        }
     }
 
 }
