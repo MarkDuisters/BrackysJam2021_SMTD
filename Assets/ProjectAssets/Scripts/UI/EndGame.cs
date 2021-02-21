9d8bef1029f0ffd250ecb006a0002533985bde02
Mark <freak_mania@hotmail.com>
2021-02-21 02:08:39 +0100

    Added proper ending

---
+++ Assets/ProjectAssets/Scripts/UI/EndGame.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EndGame : MonoBehaviour
+{
+    public GameObject playerContainer;
+    public GameObject enemyContainer;
+    public GameObject endCredits;
+    // Start is called before the first frame update
+    void OnTriggerEnter (Collider col)
+    {
+        if (col.tag == "Player")
+        {
+
+            Destroy (enemyContainer);
+            Destroy (playerContainer);
+
+        }
+
+    }
+}
